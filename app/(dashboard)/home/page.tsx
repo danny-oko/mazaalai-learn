9cac017f7e66b3bbdb58449de5ec7fda0100296e
b4yrra <t.chinbayr@gmail.com>
2026-04-22 15:04:57 +0800

    Home Page Hiigegui

---
+++ app/(dashboard)/home/page.tsx
@@ -0,0 +1,10 @@
+import "@fontsource/plus-jakarta-sans";
+import { HomePath } from "./_components/HomeSectionPath";
+
+export default function HomeSection() {
+  return (
+    <div className="flex justify-center bg-[#F0EDE3] md:py-10 min-h-screen font-['Plus_Jakarta_Sans']">
+      <HomePath />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,5 @@
 const Home = () => {
-  return (
-    <div>
-      <div>Hello this is main home</div>
-      <p>hi</p>
-      <p>Text here and title</p>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
