369fef40181940af4394dd7eef8a819667e85801
Sukh-Ochir927 <batjargalsukhochir27@gmail.com>
2026-04-27 11:23:12 +0800

    + AlphabetCard

---
+++ app/(dashboard)/dictionary/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AlphabetChart from "./components/AlphabetChart";
+
+const Alphabet = () => {
+  return (
+    <div>
+      <AlphabetChart />
+    </div>
+  );
+};
+
+export default Alphabet;
