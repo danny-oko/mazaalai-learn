c2117b4275b73e78d41bb71fdd3095af471912a8
“Oyunjargal17” <oyunjargal.teacher@gmail.com>
2026-04-29 10:30:31 +0800

    Done Payload

---
+++ prisma/Payload/Sections/sections.ts
@@ -0,0 +1,40 @@
+[
+  {
+    id: "cmoi2gy340000ic2h5jlo3edx",
+    title: "Basics 1",
+    order: 1,
+    lessons: [
+      {
+        id: "cmoi2jetp0001ic2h190oebar",
+        title: "Эгшиг үсгүүд",
+        description: "Монгол эгшиг үсгүүдийг сурах",
+        order: 1,
+        isCompleted: false,
+        levelId: "cmoi2gy340000ic2h5jlo3edx",
+      },
+    ],
+  },
+  {
+    id: "cmoihchgd0002c22hpnc65sbn",
+    title: "Хичээл 1",
+    order: 2,
+    lessons: [
+      {
+        id: "cmoii3wk30003c22hivi93ox1",
+        title: "Хичээл 1 Үндсэн махбод",
+        description: "МОНГОЛ БИЧГИЙН ҮСГИЙН ҮНДСЭН МАХБОД",
+        order: 1,
+        isCompleted: false,
+        levelId: "cmoihchgd0002c22hpnc65sbn",
+      },
+      {
+        id: "cmoijbyum000xc22hztpl7ixc",
+        title: "Хичээл 2 Үндсэн махбод",
+        description: "МОНГОЛ БИЧГИЙН ҮСГИЙН ҮНДСЭН МАХБОД",
+        order: 2,
+        isCompleted: false,
+        levelId: "cmoihchgd0002c22hpnc65sbn",
+      },
+    ],
+  },
+];
