89e234d7d33e48873b5154ec80b2722af9c3bbe5
Jagaa Zaya <raycharles@Jagaas-MacBook-Air.local>
2026-05-02 18:42:27 +0800

    UI Design Changed

--- app/_components/Bar-Sections/side-bar.tsx
+++ app/_components/Bar-Sections/side-bar.tsx
@@ -31,36 +31,35 @@ export const SideBar = ({ menuLabels, menuPaths }: HeaderClientProps) => {
 
   return (
     <aside
-      className="fixed top-0 left-0 z-40 hidden h-full flex-col md:flex md:w-20 lg:w-[300px] border-r-2 border-[#d6d4d4] px-3 py-5"
+      className="fixed bg-[#f8f4e3] top-0 left-0 z-40 hidden h-full flex-col border-r-2 border-[#d6d4d4] py-5 md:flex md:w-20 md:px-2 lg:w-[260px] lg:px-3"
       aria-label="Main navigation"
     >
-      <div className="flex h-[60px] items-center justify-center px-5 lg:justify-between">
+      <div className="flex h-[60px] items-center justify-center px-2 lg:justify-start lg:px-5">
         <img
           src="https://d35aaqx5ub95lt.cloudfront.net/vendor/0cecd302cf0bcd0f73d51768feff75fe.svg"
           alt="Mazaalai Learn"
-          className="block object-contain md:block lg:hidden w-50 h-50"
+          className="block h-8 w-auto object-contain md:h-9 lg:hidden"
         />
         <img
           src="https://d35aaqx5ub95lt.cloudfront.net/vendor/70a4be81077a8037698067f583816ff9.svg"
           alt="Mazaalai Learn"
-          className="block object-contain md:hidden lg:block w-30 h-50"
+          className="hidden h-9 w-auto object-contain lg:block"
         />
       </div>
 
-      <nav className="flex flex-1 flex-col gap-3 p-3 lg:p-4">
+      <nav className="flex flex-1 flex-col gap-3 md:px-1 lg:p-4">
         {menuLabels.map((label) => {
-          const Icon = menuIcons[label];
           const isActive = active === label;
           return (
             <button
               key={label}
               onClick={() => handleNav(label)}
-              className={`flex items-center justify-center rounded-xl px-3 py-1 text-[16px] lg:justify-start lg:gap-5 lg:px-4
+              className={`flex w-full items-center justify-center rounded-xl py-2 text-[16px] md:px-0 lg:justify-start lg:gap-5 lg:px-4
               transition-all duration-150 font-balsamiq border-2
               ${
                 isActive
                   ? "bg-[#84d8ff]/50 text-[#1cb0f6] border-[#84d8ff]"
-                  : "text-[#0F5238] hover:bg-[#F2EEDD] text-[#777777] border-transparent"
+                  : "text-[#777777] hover:bg-[#F2EEDD] border-transparent"
               }`}
             >
               <img
@@ -68,9 +67,7 @@ export const SideBar = ({ menuLabels, menuPaths }: HeaderClientProps) => {
                 alt={label}
                 width={35}
                 height={35}
-                className={`transition-all duration-200 ${
-                  isActive ? "opacity-100" : "opacity-100"
-                }`}
+                className="h-7 w-7 object-contain transition-all duration-200 md:h-8 md:w-8 lg:h-9 lg:w-9"
               />
               <div className="md:hidden lg:block uppercase text-[14px] font-extrabold tracking-wider">
                 {label}
